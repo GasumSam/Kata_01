4d053c4fe557b24d3b0d0f89ed5b6891225f10bb
GasumSam <gasumsam@gmail.com>
2020-06-08 07:15:52 +0200

    función baraja

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+palos = ('o', 'c', 'e', 'b')
+cartas = ('A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R')
+
+
+def crea_baraja():  #No necesito especificar nada porque ya está definidos, si lo hiciera los recorrería
+                        #Puede ser interesante cuando hay varias tuplas de datos
+                            #Meter parámetros cuando tenga sentido hacerlo
+
+    baraja = []
+    
+    for palo in palos:
+        for carta in cartas:
+            naipe = carta + palo
+            baraja.append(naipe)
+        
+    return baraja
+
+baraja1 = crea_baraja()
+
+print(baraja1)
